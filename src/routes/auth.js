f1eebc049ff80d63a1f97d4f54d0f5ff69d195c9
akshat1803 <sakshat678@gmail.com>
2025-01-21 23:48:39 +0530

    debugging 401

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
   try {
-    const hashedPassword = await bcrypt.hash(password, 10); 
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ username, email, password: hashedPassword });
     res.status(201).json(user);
   } catch (err) {
@@ -28,7 +28,22 @@ router.post("/login", async (req, res) => {
     if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
-    res.json({ token });
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: false, //as we are working with localhost, which runs on http, not on https
+        sameSite: "strict",
+        maxAge: 3600000,
+      })
+      .status(200)
+      .json({
+        message: "Login Successful",
+      });
+    return true;
+
+    // console.log(token);
+    // res.json({ token });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
