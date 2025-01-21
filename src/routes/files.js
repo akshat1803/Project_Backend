d9fe683180a56703592b45651943646c4eba00b1
akshat1803 <sakshat678@gmail.com>
2025-01-21 14:36:31 +0530

    backend

--- src/routes/files.js
+++ src/routes/files.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
-const File = require('../../models/File');
+const File = require('../models/File');
+
 const protect = require('../../middleware/authMiddleware');
 const router = express.Router();
 
